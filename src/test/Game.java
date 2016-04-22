8fb136e96683c7cf0f78dc793eadb9eee2b8539a
kwonnueng <kkn0313@naver.com>
2016-04-22 19:43:33 +0900

    04

---
+++ src/test/Game.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Game {
+	public Game(){}
+	int number=5;
+	String[] arr = new String[number];
+	 public void receipt(Guest g){
+
+	for(int i =0; i<arr.length;i++){
+		String name ="";
+		Scanner sc=new Scanner(System.in);
+		name=sc.nextLine();
+		Guest gName= new Guest(name);
+		String getName=g.getName();
+		//String setName=g.setName();
+		//ArrayList<Guest> guest = new ArrayList<Guest>();
+		//guest.add(gName);
+		
+	}
+
+	 }
+
+	}
---
+++ src/test/Guest.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Guest {
+
+	private String name="";
+
+	public String getName(){
+		return this.name;
+	}
+   public Guest(String name){
+      this.name=name;
+	}
+
+}
+
