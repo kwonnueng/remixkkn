862152a913ee64414c83f4a15e24a53efaeb7a9b
kwonnueng <kkn0313@naver.com>
2016-05-02 00:28:36 +0900

    완성파일

--- src/randomNumber/RandomNumber.java
+++ src/randomNumber/RandomNumber.java
@@ -15,27 +15,36 @@ public class RandomNumber {
 		//각각 랜덤으로 생성될 휴대전화번호를 담을 변수 선언
 		String phoneNum="";
 		//생성된 휴대전화 번호를 담을 배열 선언
-		String[] phoneNumArr =new String[num];
+		StringBuilder sb=new StringBuilder();
+	
 		
+		
+		String[] phoneNumArr =new String[num];
 		A:for(int i=0; i<phoneNumArr.length; i++){
-			phoneNum="010";
+			System.out.println(i);
+		
+			sb.append(phoneNum);
 			for(int k=0;k<8; k++){
 				if(k==0||k==4){
-					phoneNum=phoneNum+"-";
+						sb.append("-");
+//						phoneNum=phoneNum+"-";
 				}
 				randomNum=	(int)(Math.random()*10);
-				phoneNum=phoneNum+Integer.toString(randomNum);
-			
+//				phoneNum=phoneNum+Integer.toString(randomNum);
+				sb.append(randomNum);
 			}
-			phoneNumArr[i]=phoneNum;
-			for(int q=0; q<-1; q++){
-				if(phoneNumArr[q].equals(phoneNum)){	
+			for(int q=0; q<=i-1; q++){
+//				if(phoneNumArr[q].equals(phoneNum)){
+				if(phoneNumArr[q].equals(sb.toString())){
 					System.out.println("다시 입력받기");
-					System.out.println("다시입력받기전 현재 phoneNum = "+phoneNum);
+					System.out.println("다시입력받기전 현재 phoneNum = "+sb.toString());
 					i--;
 					continue A;
 				}
 			}
+//			phoneNumArr[i]=phoneNum;
+			phoneNumArr[i]=sb.toString();
+			sb=new StringBuilder();
 		}
 		for(int i=0;i<phoneNumArr.length;i++){
 			System.out.println((i+1)+"번째 생성된 휴대전화번호 출력="+phoneNumArr[i]);	
@@ -48,11 +57,16 @@ public class RandomNumber {
 		
 		//지역번호배열 인덱스에서 꺼내서 저장할 변수
 		String areaNum="";
+		/*
+		
 		System.out.println("지역번호 1~6까지 선택하여 입력해주세요");
 		System.out.println("1번=서울 , 2번=경기, 3번=인천, 4번=대전, 5번=광주, 6번=제주");
 		
+		*/
 		//1~6숫자를 입력받아 저장할 변수
-		int selectNum=sc.nextInt();	
+		int selectNum=0;
+		
+	/*	
 		sc.nextLine();	
 		while(0>selectNum|| selectNum>6){
 			System.out.println("다시입력하세요");
@@ -61,17 +75,21 @@ public class RandomNumber {
 			//true면 다시입력 false면 while문 빠져나오기
 		}
 		//areaNum=areaStringArr[selectNum-1];
-		
+
 		System.out.println("선택하신 번호="+selectNum+"지역번호는 "+areaNum+"입니다");
 		System.out.println("아까 입력한"+num+"갯수만큼 일반전화번호 생성합니다");
+		
+		*/
+		
+		
 		System.out.println();
 		//생성된 지역번호  담을 배열 선언
 		String[] areaPhoneNumArr=new String[num];
 		
 	B:	for(int i=0; i<areaPhoneNumArr.length; i++){
 		//지역번호 초기화
-		areaNum=areaStringArr[selectNum-1];	
-			
+		selectNum=(int)(Math.random()*6);
+		areaNum=areaStringArr[selectNum];	
 			for(int j=0; j<7; j++){
 				if(j==0||j==3){
 					areaNum=areaNum+"-";
@@ -79,8 +97,8 @@ public class RandomNumber {
 				randomNum=(int)(Math.random()*10);
 				areaNum=areaNum+Integer.toString(randomNum);
 		}
-			areaPhoneNumArr[i]=areaNum;
-			for(int k=0; k<i-1; k++){
+			
+			for(int k=0; k<=i-1; k++){
 				
 				if(areaPhoneNumArr[k].equals(areaNum)){	
 					System.out.println("같은번호가 생성됬습니다 다시 생성 하겠습니다");
@@ -88,6 +106,7 @@ public class RandomNumber {
 					continue B;
 				}
 			}
+			areaPhoneNumArr[i]=areaNum;
 		}
 		for(int i=0;i<areaPhoneNumArr.length;i++){
 			System.out.println((i+1)+"번째 생성된 지역전화번호 출력="+areaPhoneNumArr[i]);	
@@ -95,5 +114,37 @@ public class RandomNumber {
 		System.out.println("--------지역 전화번호 생성 완료--------");
 		System.out.println();
 		
-	}
+		System.out.println("070전화 만들기");
+		
+		String [] freeNumArr =new String [num];
+		String freeNum="";
+		A:for(int i=0; i<freeNumArr.length; i++){
+			freeNum="070";
+			for(int k=0;k<8; k++){
+				if(k==0||k==4){
+					freeNum=freeNum+"-";
+				}
+				randomNum=	(int)(Math.random()*10);
+				freeNum=freeNum+Integer.toString(randomNum);
+			
+			}
+		
+			for(int q=0; q<=i-1; q++){
+				if(freeNumArr[q].equals(freeNum)){	
+					System.out.println("다시 입력받기");
+					System.out.println("다시입력받기전 현재 freeNum = "+freeNum);
+					i--;
+					continue A;
+				}
+			}
+			freeNumArr[i]=freeNum;
+		}
+			for(int i=0;i<freeNumArr.length;i++){
+				System.out.println((i+1)+"번째 생성된 070번호 출력="+freeNumArr[i]);	
+			}	
+			System.out.println("--------070 전화번호 생성 완료--------");
+			System.out.println();
+			
+			
+		}
 	}
